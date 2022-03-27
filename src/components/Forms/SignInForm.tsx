f4811ba9ebbaba99525df732b93ddddb4bc52399
Fellipe Utaka <fellipeutaka@gmail.com>
2022-03-27 18:49:28 -0300

    SignInForm component

---
+++ src/components/Forms/SignInForm.tsx
@@ -0,0 +1,97 @@
+import {
+  Input,
+  Button,
+  Flex,
+  Link,
+  Text,
+  InputGroup,
+  InputLeftElement,
+  Divider,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+import { EmailIcon, LockIcon } from "@chakra-ui/icons";
+import { GoogleIcon } from "components/Icons";
+import { FormEvent, useState } from "react";
+import { useAuth } from "hooks/useAuth";
+
+export default function SignInForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { authMethods } = useAuth();
+
+  function handleSignIn(e: FormEvent) {
+    e.preventDefault();
+    setLoading(true);
+    authMethods
+      .signInWithEmailAndPassword(email, password)
+      .finally(() => setLoading(false));
+  }
+
+  return (
+    <>
+      <Flex
+        as="form"
+        flexDir="column"
+        w={["90%", "100%"]}
+        maxW="480px"
+        onSubmit={handleSignIn}
+      >
+        <InputGroup>
+          <InputLeftElement pointerEvents="none" mt="3px">
+            <EmailIcon color="gray.300" />
+          </InputLeftElement>
+          <Input
+            type="text"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            mb={6}
+            h="46px"
+          />
+        </InputGroup>
+        <InputGroup>
+          <InputLeftElement pointerEvents="none" mt="3px">
+            <LockIcon color="gray.300" />
+          </InputLeftElement>
+          <Input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            mb={2}
+            h="46px"
+          />
+        </InputGroup>
+        <NextLink passHref href="/forgot">
+          <Link w="max-content">Forgot my password</Link>
+        </NextLink>
+        <Button
+          type="submit"
+          isLoading={loading}
+          my={6}
+          w="100%"
+          h={12}
+          bgColor="blue.500"
+          _hover={{ backgroundColor: "blue.400" }}
+        >
+          Sign In
+        </Button>
+        <Text alignSelf="center">
+          Don&apos;t have an account?{" "}
+          <NextLink passHref href="/signUp">
+            <Link>Sign Up</Link>
+          </NextLink>
+        </Text>
+        <Divider my={6} />
+      </Flex>
+      <Button
+        h={12}
+        onClick={authMethods.signInWithGoogle}
+        leftIcon={<GoogleIcon w={8} h={8} />}
+      >
+        Google
+      </Button>
+    </>
+  );
+}
--- src/pages/signIn/index.tsx
+++ src/pages/signIn/index.tsx
@@ -1,34 +1,8 @@
 import Head from "next/head";
-import { FormEvent, useState } from "react";
-import { useAuth } from "hooks/useAuth";
-import {
-  Input,
-  Button,
-  Flex,
-  Heading,
-  Link,
-  Text,
-  InputGroup,
-  InputLeftElement,
-  Divider,
-} from "@chakra-ui/react";
-import NextLink from "next/link";
-import { EmailIcon, LockIcon } from "@chakra-ui/icons";
-import { GoogleIcon } from "components/Icons";
+import { Flex, Heading } from "@chakra-ui/react";
+import SignInForm from "components/Forms/SignInForm";
 
 export default function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
-  const { authMethods } = useAuth();
-
-  function handleSignIn(e: FormEvent) {
-    e.preventDefault();
-    setLoading(true);
-    authMethods
-      .signInWithEmailAndPassword(email, password)
-      .finally(() => setLoading(false));
-  }
   return (
     <>
       <Head>
@@ -48,68 +22,7 @@ export default function SignIn() {
         <Heading as="h1" mb={6}>
           Sign In
         </Heading>
-        <Flex
-          as="form"
-          flexDir="column"
-          w={["90%", "100%"]}
-          maxW="480px"
-          onSubmit={handleSignIn}
-        >
-          <InputGroup>
-            <InputLeftElement pointerEvents="none" mt="3px">
-              <EmailIcon color="gray.300" />
-            </InputLeftElement>
-            <Input
-              type="text"
-              placeholder="E-mail"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              mb={6}
-              h="46px"
-            />
-          </InputGroup>
-          <InputGroup>
-            <InputLeftElement pointerEvents="none" mt="3px">
-              <LockIcon color="gray.300" />
-            </InputLeftElement>
-            <Input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              mb={2}
-              h="46px"
-            />
-          </InputGroup>
-          <NextLink passHref href="/forgot">
-            <Link w="max-content">Forgot my password</Link>
-          </NextLink>
-          <Button
-            type="submit"
-            isLoading={loading}
-            my={6}
-            w="100%"
-            h={12}
-            bgColor="blue.500"
-            _hover={{ backgroundColor: "blue.400" }}
-          >
-            Sign In
-          </Button>
-          <Text alignSelf="center">
-            Don&apos;t have an account?{" "}
-            <NextLink passHref href="/signUp">
-              <Link>Sign Up</Link>
-            </NextLink>
-          </Text>
-          <Divider my={6} />
-        </Flex>
-        <Button
-          h={12}
-          onClick={authMethods.signInWithGoogle}
-          leftIcon={<GoogleIcon w={8} h={8} />}
-        >
-          Google
-        </Button>
+        <SignInForm />
       </Flex>
     </>
   );
